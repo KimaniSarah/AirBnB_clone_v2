6d3d73fa87a29e35cd8d982a6fc9f3541c194cce
KimaniSarah <bibokimani@gmailcom>
2024-01-15 11:50:21 -0800

    DBStorage - Amenity... and BOOM

--- models/user.py
+++ models/user.py
@@ -4,17 +4,17 @@ from models.base_model import BaseModel, Base, Column, String
 from sqlalchemy.orm import relationship
 import os
 
+
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
-        password: password for your login
+        password: password for you login
         first_name: first name
         last_name: last name
     """
 
     __tablename__ = 'users'
-
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
