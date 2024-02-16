1a9eda04b9214d2b03c8d84aadf9e02abd06cfc1
KimaniSarah <bibokimani@gmailcom>
2024-02-16 08:33:22 -0800

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display_hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    return "HBNB"
+
+
+@app.route("//c/<text>", strict_slashes=False)
+def display_c(text):
+    string = text.replace('_', ' ')
+    return f"C {string}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def display_python(text="is_cool"):
+    string = text.replace('_', ' ')
+    return f"Python {string}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_num(n):
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def display_html(n):
+    if isinstance(n, int):
+        return render_template("5-number.html", num=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
